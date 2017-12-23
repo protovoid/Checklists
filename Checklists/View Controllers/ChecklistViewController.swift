fc7a1664941256b6d7afcabb60b933acf1360647
Chad Williams <fadewave@gmail.com>
2017-12-22 17:36:17 -0800

    icons, fixed layout issues (chap 19)

--- Checklists/View Controllers/ChecklistViewController.swift
+++ Checklists/View Controllers/ChecklistViewController.swift
@@ -110,6 +110,7 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     
   func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
     let label = cell.viewWithTag(1001) as! UILabel
+    label.textColor = view.tintColor
     
       if item.checked {
         label.text = "√"
